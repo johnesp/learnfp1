f077ba285af221d358ac08c6f31e2e82572f5c3b
JohnE <jespinosa@grupoesoft.com>
2015-11-17 18:57:55 -0500

    immutable working, booksnipet2 in place

--- app/components/fplearning1/booksnipet2.ts
+++ app/components/fplearning1/booksnipet2.ts
@@ -7,25 +7,8 @@ export class BookSnipet2 {
   }
   
   public executeCode() {
-    console.log('Starting snipet 2...!');
-    var decrementHP = function(player) {
-      return player.set('hp', player.get('hp')-1);
-    };    
-    var isSameTeam = function(player1, player2) {
-      return player1.get('team') === player2.get('team');
-    };    
-    var punch = function(player, target) {
-      if (isSameTeam(player, target)) {
-        return target;
-      } else {
-        return decrementHP(target);
-      }
-    };    
-    var michael = Map({name:'Michael', hp:20, team: 'green'});    
-    var jobe = Immutable.Map({name:'Jobe', hp:20, team: 'red'});
     
-    console.log(punch(jobe, michael));
-    //returns:
-    //=> Immutable.Map({name:"Michael", hp:19, team: "green"})
-    }
+  }
+  
+    
 }
